8deac2f107942e5e320fd64e3a886bb88e50c6a6
Andrew Fernandes <andrew@fernandes.org>
2015-10-14 20:05:11 -0400

    Use 'sh' rather than 'bash' for compatibility with busybox and boot2docker

--- export.go
+++ export.go
@@ -531,7 +531,7 @@ func (e *Export) TarLayers(w io.Writer) error {
 	}
 	defer os.Chdir(cwd)
 
-	cmd := exec.Command("sudo", "/bin/bash", "-c", "tar cOf  - *")
+	cmd := exec.Command("sudo", "/bin/sh", "-c", "tar cOf  - *")
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return err
